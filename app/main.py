4559e31d64606b0f1663f710a9f1c6aaa82d552a
Dmytro Hrabovskyi <dima.grabovskiy2000@gmail.com>
2024-09-11 10:20:47 +0300

    correction lines 7, 15 and rename arguments in format_linter_report

--- app/main.py
+++ app/main.py
@@ -4,16 +4,18 @@ def format_linter_error(error: dict) -> dict:
         "column": error.get("column_number"),
         "message": error.get("text"),
         "name": error.get("code"),
-        "source": "flake8"}
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": ("failed" if errors else "passed")}
+        "status": ("failed" if errors else "passed")
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value)
-            for key, value in linter_report.items()]
+    return [format_single_linter_file(file, report)
+            for file, report in linter_report.items()]
